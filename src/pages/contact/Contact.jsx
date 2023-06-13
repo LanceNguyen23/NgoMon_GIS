734766325fd852f2654f40a1eebff45e524a6357
NguyenTraVi <19522512@gm.uit.edu.vn>
2023-06-13 13:43:38 +0700

    done contact

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,5 +1,54 @@
+import "../contact/Contact.css";
+
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="contact">
+      <h1 className="contact-title">Liên Hệ</h1>
+      <div className="contact-contain">
+        <form className="contact-contain-form">
+          <div className="contact-form">
+            <label className="contact-form-title">Họ và tên:</label>
+            <input className="contact-form-input" type="text" name="fullname" />
+          </div>
+
+          <div className="contact-form">
+            <label className="contact-form-title">Số điện thoại:</label>
+            <input
+              className="contact-form-input"
+              type="tel"
+              name="numberphone"
+            />
+          </div>
+
+          <div className="contact-form">
+            <label className="contact-form-title">Email:</label>
+            <input className="contact-form-input" type="email" name="email" />
+          </div>
+
+          <div className="contact-form">
+            <label className="contact-form-title">Chủ đề:</label>
+            <select
+              className="contact-form-input"
+              id="selectField"
+              name="reason"
+            >
+              <option value="modal3D">Đóng góp thông tin di tích 3D</option>
+              <option value="web">Góp ý trang web</option>
+              <option value="more">Khác</option>
+            </select>
+          </div>
+
+          <div className="contact-form">
+            <label className="contact-form-title">Nội dung:</label>
+            <textarea
+              className="contact-form-textarea"
+              type="text"
+              name="message"
+            />
+          </div>
+          <button className="submit">Gửi</button>
+        </form>
+      </div>
+    </div>
+  );
 }
